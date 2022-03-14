34e2f5b9585200fca2ace33be3e3e65e538512b2
Joel Thompson <jt10280@georgiasouthern.edu>
2022-03-14 18:26:04 -0400

    Created simple rest api and initialized react

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+app.use(express.json());
+
+app.listen(
+    PORT,
+    () => console.log(`being hosted on  http://localhost:${PORT}`)
+)
+
+app.get('/hello', (req, res) =>  {
+    res.status(200).send({
+        text: 'Hello this api was called'
+
+    })
+ });
+
+ app.post('/hello/respond', (req, res) => {
+    const { response } = req.body;
+
+    if(!response) {
+        res.status(418).send({message: 'you didnt respond!' })
+    }
+    res.send({
+        reply: `${response}`
+    })
+    })
