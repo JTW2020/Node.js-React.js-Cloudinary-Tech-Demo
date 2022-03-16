00088964bd69620869d571a971995a87d0f89da3
Joel Thompson <jt10280@georgiasouthern.edu>
2022-03-15 20:35:43 -0400

    able to upload image to cloudinary and view it on the page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
+import Gallery from './Gallery';
 
 function App() {
+
+  const [loading,setLoading] = useState(false)
+  const [image, SetImage] = useState("")
+
+  const uploadImage = async e => {
+    const files = e.target.files
+    const data = new FormData()
+    data.append('file',files[0])
+    data.append('upload_preset','cloudinarytest')
+    setLoading(true)
+
+    const res = await fetch("https://api.cloudinary.com/v1_1/soaringeaglesspring2022/image/upload",
+    {
+      method:'POST',
+      body:data
+    })
+
+    const file = await res.json()
+
+    console.log(file)
+
+    SetImage(file.secure_url)
+    setLoading(false)
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <Gallery />
+      <input type="file" id="file" name="file" placeholder='Upload an image' onChange={uploadImage}/>
+
+      {
+        loading?(
+        <h3>Loading ...</h3>
+        ):(
+        <img src={image} style = {{width:'300px'}}/>
+        )
+      }
+  
+    </>
   );
 }
 
